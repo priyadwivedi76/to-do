83577a5f958ef72c8df7132c21b333e379a97ab0
Dwivedi Priya <pd7668665@gmail.com>
2025-04-02 01:04:41 +0530

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import React from "react";
-import { Route,Routes,Navigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import FormTask from "./components/FormTask";
 import Details from "./components/Details";
 import Weather from "./components/Weather";
 import Signup from "./components/SignUp";
 import Login from "./components/Login";
-import { useState,useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import "./App.css"; // Ensure you create a styles.css file for styling
+import "./App.css"; // Ensure you have your styles
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const authStatus = sessionStorage.getItem("isAuthenticated");
     setIsAuthenticated(authStatus === "true");
   }, []);
 
-  return(
-    <>
-    <Routes>
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={isAuthenticated ? <Home setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/login" />} />
-      <Route path="/form" element={<FormTask />} />
-      <Route path='/learn/:id' element={<Details/>}/>
-      <Route path='/weather' element={<Weather/>}/>
-    </Routes>
-    </>
-  )
+  return (
+    <Router> {/* Wrap everything inside HashRouter */}
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={isAuthenticated ? <Home setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/login" />} />
+        <Route path="/form" element={<FormTask />} />
+        <Route path="/learn/:id" element={<Details />} />
+        <Route path="/weather" element={<Weather />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
 
